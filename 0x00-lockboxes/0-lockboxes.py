9d14e07c0ea939054bdc9a889791d5e3c94516b6
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-12-08 14:08:48 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,19 +2,20 @@
 """determines if all the boxes can be opened
 """
 
+
 def canUnlockAll(boxes):
-    LK=[]
-    LB=[]
-    a=0
-    for i in range(len(boxes) -1):
+    LK = []
+    LB = []
+    a = 0
+    for i in range(len(boxes) - 1):
         LB.append(i+1)
     LK.extend(boxes[0])
-    T=1
-    B=7
-    Z=LK[0:len(LK)]
-    while len(LB) !=0 and T!=B:
-        Z=LK[0:len(LK)]
-        T=len(LB)
+    T = 1
+    B = 7
+    Z = LK[0:len(LK)]
+    while len(LB) != 0 and T != B:
+        Z = LK[0:len(LK)]
+        T = len(LB)
         for i in Z:
             if i in LB:
                 LK.extend(boxes[i])
@@ -22,8 +23,8 @@ def canUnlockAll(boxes):
                 LB.pop(LB.index(i))
             else:
                 True
-        B=len(LB)
-    if len(LB)==0:
+        B = len(LB)
+    if len(LB) == 0:
         print("True")
     else:
         print("False")
