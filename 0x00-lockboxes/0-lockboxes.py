ae88d03cbb1d302c2fa0c35562413d1353b8181c
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-12-08 14:01:41 +0100

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""determines if all the boxes can be opened
+"""
+
+def canUnlockAll(boxes):
+    LK=[]
+    LB=[]
+    a=0
+    for i in range(len(boxes) -1):
+        LB.append(i+1)
+    LK.extend(boxes[0])
+    T=1
+    B=7
+    Z=LK[0:len(LK)]
+    while len(LB) !=0 and T!=B:
+        Z=LK[0:len(LK)]
+        T=len(LB)
+        for i in Z:
+            if i in LB:
+                LK.extend(boxes[i])
+            if i in LB:
+                LB.pop(LB.index(i))
+            else:
+                True
+        B=len(LB)
+    if len(LB)==0:
+        print("True")
+    else:
+        print("False")
