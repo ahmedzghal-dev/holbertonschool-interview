eac8986ba418e340eb69db9f04ee7b1a952bb07f
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-12-08 18:06:41 +0100

    0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,7 +2,7 @@
 """determines if all the boxes can be opened
 """
 
-
+"""
 def canUnlockAll(boxes):
     LK = []
     LB = []
@@ -27,3 +27,15 @@ def canUnlockAll(boxes):
         print("True")
     else:
         print("False")
+"""
+def canUnlockAll(boxes):
+unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
