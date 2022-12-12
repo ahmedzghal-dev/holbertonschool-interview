9c00007926c9794e70a456bd09fffad52e0cae64
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-12-12 23:51:36 +0100

    c

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+
+ listint_t *insert_node(listint_t **head, int number){
+    
+ }
