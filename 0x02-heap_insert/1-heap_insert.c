bcbee784aa1b45c5cf189a17e676d9a1cc81eaf5
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-12-22 18:23:55 +0100

    1

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * heap_insert - .....
+ * @root: ....
+ * @value: ....
+ * Return: ....
+ *
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+    while (root->n < value)
+    {
+        swap(&root, )
+    }
+}
+
+/**
+ * swap - .....
+ * @y: ....
+ * @x: ....
+ * Return: ....
+ *
+ */
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
