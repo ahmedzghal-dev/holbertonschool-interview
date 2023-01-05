b33d9e40edbf792343d573144ffe642b20371d1f
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-01-05 22:11:51 +0100

    0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,18 +9,14 @@ needed to result in exactly n H characters in the file.
 def minOperations(n):
     counter = 0
     i = 1
-    while (n):
-        while i < n:
-            if (i % 2 != 0):
-                a = i
-                counter = counter + 2
-                i = i * 2
-                if i + (a * 2) == n:
-                    counter = counter + 2
-                    return counter
-            if (i % 2 == 0):
-                counter = counter + 1
-                i = i + a
-                a = i
-        return counter
-    return 0
+    a = 0
+    while i < n:
+        if n % i == 0:
+            a = i
+            counter = counter + 1
+            i = i * 2
+        else:
+            i += a
+        counter += 1
+
+    return counter
