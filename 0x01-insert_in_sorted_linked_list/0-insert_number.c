fc3057a35e53075736122e2d6fa0ba1fc8fc9354
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-09 22:23:54 +0100

    0

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,6 +9,24 @@
  * Return: address of the new element or NULL if it fails
  */
 
- listint_t *insert_node(listint_t **head, int number){
-    
- }
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t **current;
+
+	current = head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+
+		while ((*current) != NULL && (*current)->n < number)
+		{
+			current = &(*current)->next;
+		}
+	new->next = (*current);
+	(*current) = new;
+	return (new);
+}
