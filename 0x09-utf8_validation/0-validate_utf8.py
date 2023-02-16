a47a4a064cbb596ffcf45b8da9c87cc12382a59e
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-16 18:27:30 +0100

    0

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -13,41 +13,26 @@ Returns:
 
 
 def validUTF8(data):
-    num_bytes = 0
-    for byte in data:
-        # If the most significant bit is 0, this is a single-byte character
-        if byte >> 7 == 0:
-            if num_bytes != 0:
-                return False
-        # If the most significant bits are 110, this is a two-byte character
-        elif byte >> 5 == 0b110:
-            if num_bytes != 0:
-                return False
-            num_bytes = 1
-        # If the most significant bits are 1110, this is a three-byte character
-        elif byte >> 4 == 0b1110:
-            if num_bytes != 0:
-                return False
-            num_bytes = 2
-        # If the most significant bits are 11110, this is a four-byte character
-        elif byte >> 3 == 0b11110:
-            if num_bytes != 0:
-                return False
-            num_bytes = 3
+    """
+    Returns:
+    bool: True if the data set is a valid
+    UTF-8 encoding, False otherwise.
+    """
+    i = 0
+    while i < len(data):
+        count = 0
+        mask = 1 << 7
+        while mask & data[i]:
+            count += 1
+            mask >>= 1
+        if count == 0:
+            pass
+        elif count == 1 or count > 4:
+            return False
         else:
-            # Invalid most significant bits
-            return False
-
-        # For each additional byte in a multi-byte character,
-        # the two most significant bits must be 10
-        if num_bytes > 0:
-            num_bytes -= 1
-            if byte >> 6 != 0b10:
-                return False
-
-        # If there are still bytes left in a multi-byte character,
-        # this is an invalid encoding
-        if num_bytes > 0:
-            return False
-
-        return True
+            for j in range(i + 1, i + count):
+                if j >= len(data) or (data[j] >> 6) != 2:
+                    return False
+            i += count - 1
+        i += 1
+    return True
