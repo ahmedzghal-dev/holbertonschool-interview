5a7a63dcc5c870f1b24479828ec02651d6f3adb5
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-26 19:41:52 +0100

    0

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,96 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "slide_line.h"
-#define SLIDE_LEFT 0
-#define SLIDE_RIGHT 1
+
+#define LINE_SIZE   32
 
 /**
- * slide_line - slides and merges an array of integers
- * @line: pointer to array of integers
- * @size: number of elements in @line
- * @direction: direction to slide and merge (@SLIDE_LEFT or @SLIDE_RIGHT)
- *
- * Return: 1 on success, 0 on failure
+ * print_array - Prints out an array of integer, followed by a new line
+ * 
+ * @array: Pointer to the array of integer to be printed
+ * @size: Number of elements in @array
  */
-int slide_line(int *line, size_t size, int direction)
+static void print_array(int const *array, size_t size)
 {
-    size_t i, j, k;
+    size_t i;
 
-    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
-        return 0;
+    printf("Line: ");
+    for (i = 0; i < size; i++)
+        printf("%s%d", i > 0 ? ", " : "", array[i]);
+    printf("\n");
+}
 
-    if (direction == SLIDE_LEFT)
+/**
+ * main - Entry point
+ *
+ * @ac: Arguments counter
+ * @av: Arguments vector
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int ac, char **av)
+{
+    int line[LINE_SIZE];
+    int direction;
+    size_t i, size;
+
+    if (ac < 3)
     {
-        for (i = 0, j = 0; i < size; i++)
-        {
-            if (line[i] != 0)
-            {
-                if (i != j)
-                {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j++;
-            }
-        }
-
-        for (i = 0; i < size - 1; i++)
-        {
-            if (line[i] > 0 && line[i] == line[i + 1])
-            {
-                line[i] *= 2;
-                line[i + 1] = 0;
-            }
-        }
-
-        for (i = 0, j = 0; i < size; i++)
-        {
-            if (line[i] != 0)
-            {
-                if (i != j)
-                {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j++;
-            }
-        }
-    }
-    else
-    {
-        for (i = size - 1, j = size - 1; (int)i >= 0; i--)
-        {
-            if (line[i] != 0)
-            {
-                if (i != j)
-                {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j--;
-            }
-        }
-
-        for (i = size - 1; i > 0; i--)
-        {
-            if (line[i] > 0 && line[i] == line[i - 1])
-            {
-                line[i] *= 2;
-                line[i - 1] = 0;
-            }
-        }
-
-        for (i = size - 1, j = size - 1; (int)i >= 0; i--)
-        {
-            if (line[i] != 0)
-            {
-                if (i != j)
-                {
-                    line[j] = line[i];
-                    line[i] = 0;
-                }
-                j--;
-            }
-        }
+        fprintf(stderr, "Usage: %s <R/L> <n1> [n2...]\n", av[0]);
+        return (EXIT_FAILURE);
     }
 
-    return 1;
+    size = ac - 2;
+    if (size > LINE_SIZE)
+        size = LINE_SIZE;
+    for (i = 0; i < size; i++)
+        line[i] = atoi(av[i + 2]);
+
+    print_array(line, size);
+
+    switch (*(av[1]))
+    {
+    case 'L':
+        direction = SLIDE_LEFT;
+        printf("Slide to the left\n");
+        break;
+    case 'R':
+        direction = SLIDE_RIGHT
+        printf("Slide to the right\n");
+        break;
+    default:
+        fprintf(stderr, "Unknown direction '%c'. Please use 'L' or 'R'", *(av[1]));
+        return (EXIT_FAILURE);
+    }
+
+    if (!slide_line(line, size, direction))
+    {
+        fprintf(stderr, "Failed to slide and merge line\n");
+        return (EXIT_FAILURE);
+    }
+
+    print_array(line, size);
+
+    return (EXIT_SUCCESS);
 }
