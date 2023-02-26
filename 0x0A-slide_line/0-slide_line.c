511ddc581496268a89d083ae9b84582559e883ba
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-02-26 19:39:46 +0100

    0

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -17,10 +17,14 @@ int slide_line(int *line, size_t size, int direction)
     if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
         return 0;
 
-    if (direction == SLIDE_LEFT) {
-        for (i = 0, j = 0; i < size; i++) {
-            if (line[i] != 0) {
-                if (i != j) {
+    if (direction == SLIDE_LEFT)
+    {
+        for (i = 0, j = 0; i < size; i++)
+        {
+            if (line[i] != 0)
+            {
+                if (i != j)
+                {
                     line[j] = line[i];
                     line[i] = 0;
                 }
@@ -28,26 +32,36 @@ int slide_line(int *line, size_t size, int direction)
             }
         }
 
-        for (i = 0; i < size - 1; i++) {
-            if (line[i] > 0 && line[i] == line[i + 1]) {
+        for (i = 0; i < size - 1; i++)
+        {
+            if (line[i] > 0 && line[i] == line[i + 1])
+            {
                 line[i] *= 2;
                 line[i + 1] = 0;
             }
         }
 
-        for (i = 0, j = 0; i < size; i++) {
-            if (line[i] != 0) {
-                if (i != j) {
+        for (i = 0, j = 0; i < size; i++)
+        {
+            if (line[i] != 0)
+            {
+                if (i != j)
+                {
                     line[j] = line[i];
                     line[i] = 0;
                 }
                 j++;
             }
         }
-    } else {
-        for (i = size - 1, j = size - 1; (int)i >= 0; i--) {
-            if (line[i] != 0) {
-                if (i != j) {
+    }
+    else
+    {
+        for (i = size - 1, j = size - 1; (int)i >= 0; i--)
+        {
+            if (line[i] != 0)
+            {
+                if (i != j)
+                {
                     line[j] = line[i];
                     line[i] = 0;
                 }
@@ -55,16 +69,21 @@ int slide_line(int *line, size_t size, int direction)
             }
         }
 
-        for (i = size - 1; i > 0; i--) {
-            if (line[i] > 0 && line[i] == line[i - 1]) {
+        for (i = size - 1; i > 0; i--)
+        {
+            if (line[i] > 0 && line[i] == line[i - 1])
+            {
                 line[i] *= 2;
                 line[i - 1] = 0;
             }
         }
 
-        for (i = size - 1, j = size - 1; (int)i >= 0; i--) {
-            if (line[i] != 0) {
-                if (i != j) {
+        for (i = size - 1, j = size - 1; (int)i >= 0; i--)
+        {
+            if (line[i] != 0)
+            {
+                if (i != j)
+                {
                     line[j] = line[i];
                     line[i] = 0;
                 }
