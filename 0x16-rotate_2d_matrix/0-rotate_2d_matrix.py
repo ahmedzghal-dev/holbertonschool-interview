734e4aa5fe28cc95026d93c45bd0446f2f23aa0d
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-06-01 11:42:41 +0100

    0

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,17 +12,13 @@ def rotate_2d_matrix(matrix):
             matrix to rotate
             can be assumed to have 2 dimensions and will not be empty
     """
-    N = len(matrix)
+    n = len(matrix)
 
-    matrix_copy = []
-    copy_row = 0
-    for column in range(N):
-        for row in range((N - 1), -1, -1):
-            if column is 0:
-                matrix_copy.append([])
-            matrix_copy[copy_row].append(matrix[row][column])
-        copy_row += 1
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    for row in range(N):
-        for column in range(N):
-            matrix[row][column] = matrix_copy[row][column]
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
