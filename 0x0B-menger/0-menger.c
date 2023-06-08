39101e79cf76d013e0c342731550046270ceef3a
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-06-08 02:54:05 +0100

    0

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,33 @@
+#include "menger.h"
+
+/**
+ * menger - draws a 2D Menger Sponge
+ * @level: level of the menger sponge to draw
+ *
+ */
+void menger(int level)
+{
+    int i, j, result, ii, jj;
+    char simbol;
+
+    result = pow(3, level);
+
+    for (i = 0; i < result; i++)
+    {
+        for (j = 0; j < result; j++)
+        {
+            simbol = '#';
+            ii = i;
+            jj = j;
+            while (ii > 0)
+            {
+                if (ii % 3 == 1 && jj % 3 == 1)
+                    simbol = ' ';
+                ii /= 3;
+                jj /= 3;
+            }
+            printf("%c", simbol);
+        }
+        printf("\n");
+    }
+}
