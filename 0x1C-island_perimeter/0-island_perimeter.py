0b7060cb5bf280edcf620723e7414e8cb1ddba17
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-07-27 12:48:40 +0100

    0-island_perimeter

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,26 +5,18 @@
 
 
 def island_perimeter(grid):
-    if not grid or not grid[0]:
-        return 0
-
-    rows, cols = len(grid), len(grid[0])
     perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
 
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-
-                if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-
-                if i == rows - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
